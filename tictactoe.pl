7459f044fe0a472d6dde05556689e82a975cdd05
siahuat0727 <tansiahuat@gmail.com>
2018-05-19 10:24:23 +0800

    done tictactoe

--- tictactoe.pl
+++ tictactoe.pl
@@ -43,6 +43,16 @@ value(Board,V) :-
    length(MIN,Vmin),      % # lines open to x
    V is Vmax - Vmin.
 
+move(Player, (1,1), [_Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z9], [Player, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z9]).
+move(Player, (1,2), [Z1, _Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z9], [Z1, Player, Z3, Z4, Z5, Z6, Z7, Z8, Z9]).
+move(Player, (1,3), [Z1, Z2, _Z3, Z4, Z5, Z6, Z7, Z8, Z9], [Z1, Z2, Player, Z4, Z5, Z6, Z7, Z8, Z9]).
+move(Player, (2,1), [Z1, Z2, Z3, _Z4, Z5, Z6, Z7, Z8, Z9], [Z1, Z2, Z3, Player, Z5, Z6, Z7, Z8, Z9]).
+move(Player, (2,2), [Z1, Z2, Z3, Z4, _Z5, Z6, Z7, Z8, Z9], [Z1, Z2, Z3, Z4, Player, Z6, Z7, Z8, Z9]).
+move(Player, (2,3), [Z1, Z2, Z3, Z4, Z5, _Z6, Z7, Z8, Z9], [Z1, Z2, Z3, Z4, Z5, Player, Z7, Z8, Z9]).
+move(Player, (3,1), [Z1, Z2, Z3, Z4, Z5, Z6, _Z7, Z8, Z9], [Z1, Z2, Z3, Z4, Z5, Z6, Player, Z8, Z9]).
+move(Player, (3,2), [Z1, Z2, Z3, Z4, Z5, Z6, Z7, _Z8, Z9], [Z1, Z2, Z3, Z4, Z5, Z6, Z7, Player, Z9]).
+move(Player, (3,3), [Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, _Z9], [Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Player]).
+
 alpha_beta(_Player,0,Board,_Alpha,_Beta,_NoMove,Value) :- 
    value(Board,Value).
 
